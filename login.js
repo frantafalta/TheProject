0526e888f96c969eb32b481ac41fdfbc4f1a5480
frantafalta <franta.falta@gmail.com>
2016-04-18 14:34:53 +0200

    better alert
    instead of popup window now simple message

--- login.js
+++ login.js
@@ -1,11 +1,19 @@
 //ověření správnosti hesla
 function login(heslo) {
     var heslo = document.getElementById("heslo").value;  //zjistí hodnotu hesla
+    //var heslo2 = MD5(heslo);
     if (heslo == "1234") {
         window.location.replace("interface.html");
     }
     else {
-        alert("Špatně");
+        heslo = null;
+        var node = document.getElementById("error");
+        while (node.firstChild) {
+            node.removeChild(node.firstChild);
+        }
+        var message = document.createTextNode("Špatně");
+        node.appendChild(message);
+                        
     }
-    return 0;
+    return null;
 }
