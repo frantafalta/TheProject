ae64a128421b15e70c38532760b335339cac8619
frantafalta <franta.falta@gmail.com>
2016-04-18 10:36:27 +0200

    +file

---
+++ login.js
@@ -0,0 +1,8 @@
+var heslo = document.getElementById("heslo").value;   //načtení hodnoty z inputu
+function login(heslo) {
+ if (heslo == "1234"){
+  window.location.replace("interface.html")
+ }
+
+
+}
