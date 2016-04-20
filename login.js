4508007333a6911a79270c2d665cabefb36cc7f2
frantafalta <franta.falta@gmail.com>
2016-04-20 18:03:04 +0200

    added database search

--- login.js
+++ login.js
@@ -1,18 +1,25 @@
 //ověření správnosti hesla
 function login(heslo) {
     var heslo = document.getElementById("heslo").value;  //zjistí hodnotu hesla
-    var heslo2 = MD5(heslo);
-    if (heslo2 == "81dc9bdb52d04dc20036dbd8313ed055") {
-        window.location.replace("interface.html");
-    }
-    else {
+    //var heslo2 = MD5(heslo);
+    
+    var i = 0;
+    // prohledání databáze
+    for (i = 0; users[i]["password"] != heslo; ++i);
+
+    window.location.replace("interface.html");
+    
+        // výpis chyby při zadání špatného hesla
+   /*
         heslo = null;
+        alert("Špatně");
+      
         var node = document.getElementById("error");
         while (node.firstChild) {
             node.removeChild(node.firstChild);
         }
         var message = document.createTextNode("Špatně");
-        node.appendChild(message);
-     }
-    return null;
+        node.appendChild(message);*/
+     
+    return users[i];
 }
