567dc128bf47bba6cca29f810dd261b6316fbfbf
frantafalta <franta.falta@gmail.com>
2016-04-20 18:06:32 +0200

    +file users.js created database

---
+++ users.js
@@ -0,0 +1,7 @@
+var users;
+users=[
+  { "firstname": "Frantisek", "lastname": "Falta", "username": "franta", "password": "1234" },
+  { "firstname": "Jan", "lastname": "Novak", "username": "jan", "password": "1111" },
+  { "firstname": "Josef", "lastname": "Omacka", "username": "josef", "password": "abcd"},
+  { "firstname": "Jenda", "lastname": "debil", "username": "hekrgay", "password": "123"}
+]
