ce7ff7e4d7cb42749f76cbe78fcec52b5126b9f8
frantafalta <franta.falta@gmail.com>
2016-04-20 18:07:22 +0200

    login.js removed error message

--- login.js
+++ login.js
@@ -1,19 +1,7 @@
 //ověření správnosti hesla
 function login(heslo) {
     var heslo = document.getElementById("heslo").value;  //zjistí hodnotu hesla
-    
-        // výpis chyby při zadání špatného hesla
-   /*
-        heslo = null;
-        alert("Špatně");
-      
-        var node = document.getElementById("error");
-        while (node.firstChild) {
-            node.removeChild(node.firstChild);
-        }
-        var message = document.createTextNode("Špatně");
-        node.appendChild(message);*/
-     
+   
     return users[i];
 
     if (heslo == "1234") {
